b3a68dbc60512ca8b163e895fc0e1eff8dc387fc
Stephan <stephan@branczyk.org>
2022-05-22 22:35:02 -0700

    Time: 208 ms (54.73%), Space: 35.9 MB (53.38%) - LeetHub

---
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.kt
@@ -0,0 +1,51 @@
+class MyQueue() {
+    // See my notes to understand the metaphor I'm using
+    private val truckBedStack: Stack<Int> = Stack()
+    private val sideOfTheRoadStack: Stack<Int> = Stack()
+
+    fun initialize() {
+        truckBedStack.clear()
+        sideOfTheRoadStack.clear()
+    }
+    
+    fun push(x: Int) {
+        // so as to not disturb FIFO mandate
+        // make sure all the meals (if any) are
+        // on the truckBedStack already
+        while (sideOfTheRoadStack.isNotEmpty()) {
+            truckBedStack.push(sideOfTheRoadStack.pop())
+        }
+        truckBedStack.push(x)
+    }
+    
+    fun unloadTruckBedStack() {
+        while (truckBedStack.isNotEmpty()) {
+            sideOfTheRoadStack.push(truckBedStack.pop());
+        }
+        return
+    }
+    
+    fun pop(): Int {
+        unloadTruckBedStack()
+        return sideOfTheRoadStack.pop()
+    }
+
+    fun peek(): Int {
+        unloadTruckBedStack()
+        return sideOfTheRoadStack.peek()
+    }
+
+    fun empty(): Boolean {
+        return (truckBedStack.isEmpty() && 
+        sideOfTheRoadStack.isEmpty());
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
