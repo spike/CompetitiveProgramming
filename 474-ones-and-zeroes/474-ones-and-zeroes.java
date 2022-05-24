a0dcc803d5516b2767705921cfa904d81f984a70
Stephan <stephan@branczyk.org>
2022-05-24 00:25:34 -0700

    Time: 427 ms (13.46%), Space: 75.1 MB (44.70%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.java
+++ 474-ones-and-zeroes/474-ones-and-zeroes.java
@@ -4,25 +4,26 @@ class Solution {
         dp = new int[z+1][o+1][strs.length];
         return helper(strs, z, o, 0);
     }
-    int helper(String[] strs, int z, int o, int index) {
-        if ((index == strs.length) || ((z + o) == 0)) {
+    private int helper(String[] strs, int z, int o, int index) {
+        if (index == strs.length || (z + o == 0)) {
             return 0;
         }
         if (dp[z][o][index] > 0) {
             return dp[z][o][index];
         }
-        int accept = 0;
+        
         int[] count = count(strs[index]);
+        int accept = 0;
         // accept
-        if ((count[0] <= z) && (count[1] <= o)) {
-            accept = 1 + helper(strs, z-count[0], o-count[1], index + 1);
+        if (count[0] <= z && count[1] <= o) {
+        accept = 1 + helper(strs, z-count[0], o-count[1], index + 1);
         }
         // skip
         int skip = helper(strs, z, o, index + 1);
         dp[z][o][index] = Math.max(accept, skip);
         return dp[z][o][index];
     }
-    int[] count(String str) {
+    private int[] count(String str) {
         int[] count = new int[2];
         for (char d : str.toCharArray()) {
             count[d-'0']++;
