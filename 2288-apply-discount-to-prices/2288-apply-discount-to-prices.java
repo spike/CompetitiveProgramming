0aac2e2753b8696762292269015503a97d80c4be
Stephan <stephan@branczyk.org>
2022-05-29 23:33:49 -0700

    Time: 770 ms (14.29%), Space: 112.6 MB (14.29%) - LeetHub

--- 2288-apply-discount-to-prices/2288-apply-discount-to-prices.java
+++ 2288-apply-discount-to-prices/2288-apply-discount-to-prices.java
@@ -1,12 +1,27 @@
-class Solution:
-    def discountPrices(self, sentence: str, discount: int) -> str:
-        words = sentence.split()
-        
-        for i in range(len(words)):
-            word = words[i]
-            if word[0] == "$" and word[1:].isnumeric():
-                amount = float(word[1:])
-                amount = amount * (100 - discount) / 100
-                words[i] = "$" + "{:.2f}".format(amount)
-        
-        return " ".join(words)
+class Solution {
+    public String discountPrices(String sentence, int discount) {
+        String[] words = sentence.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            if (word.length() < 2) continue;
+            if (word.charAt(0) != '$') continue;
+            
+            if (isValid(word.substring(1))) {
+                words[i] = "$" + format(word.substring(1), discount);    
+            }
+        }
+        return String.join(" ", words);
+    }
+    private boolean isValid(String num) {
+        for (int i = 0; i < num.length(); i++) {
+            char c = num.charAt(i);
+            if (c < '0' || c > '9') return false;
+        }
+        return true;
+    }
+    private String format(String num, int discount) {
+        double n = Long.parseLong(num) * ((100-discount) / 100d);
+        return String.format("%.2f", n);
+    } 
+    
+}
