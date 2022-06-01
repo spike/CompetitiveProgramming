4875ec11eeab1ec0fe307876331b7a971ca0b98e
Stephan <stephan@branczyk.org>
2022-05-31 21:48:23 -0700

    Time: 1 ms (57.63%), Space: 42.9 MB (6.76%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (needle.length() == 0) return 0;
+        for (int i = 0; i < haystack.length()-needle.length()+1; i++) {
+           // System.out.println(haystack.substring(i, i+needle.length()));
+            if (haystack.substring(i, i+needle.length()).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
