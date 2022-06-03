3482ade4abec33c6134870bf78dc2234e4999896
Stephan <stephan@branczyk.org>
2022-06-03 15:30:53 -0700

    Time: 879 ms (12.68%), Space: 127.4 MB (53.04%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
@@ -1,15 +1,22 @@
 class NumMatrix {
     int[][] mat;
+    HashMap<String, Integer> hm;
     public NumMatrix(int[][] matrix) {
         mat = matrix;
+        hm = new HashMap();
     }
-    
     public int sumRegion(int row1, int col1, int row2, int col2) {
         int total = 0;
-        for (int row = row1; row <= row2; row++) {
-            for (int col = col1; col <= col2; col++) {
-                total += mat[row][col];
+        String key = row1 + "-" + col1 + "=" + row2 + "-" + col2;
+        if (hm.containsKey(key)) {
+            total = hm.get(key);
+        } else {
+            for (int row = row1; row <= row2; row++) {
+                for (int col = col1; col <= col2; col++) {
+                    total += mat[row][col];
+                }
             }
+            hm.put(key, total);
         }
         return total;
     }
