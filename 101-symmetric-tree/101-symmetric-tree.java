86c464e0fa0e1ccff2c00b436165cd1e5cc16832
Stephan <stephan@branczyk.org>
2022-06-14 11:20:17 -0700

    Time: 0 ms (100.00%), Space: 40.2 MB (95.07%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -16,17 +16,14 @@
 class Solution {
     public boolean isSymmetric(TreeNode root) {
         if (root == null) return true;
-	    TreeNode subtree1 = root.left;
-	    TreeNode subtree2 = root.right;
-	    return preorderInvCheck(subtree1, subtree2);
+	    return preorderInvCheck(root.left, root.right);
     }
     boolean preorderInvCheck(TreeNode t1, TreeNode t2) {
 	    if (t1 == null && t2 == null) return true;
 	    if (t1 == null || t2 == null) return false;
 	    if (t1.val != t2.val) return false;
-	    boolean left = preorderInvCheck(t1.left, t2.right);
-	    boolean right = preorderInvCheck(t1.right, t2.left);
-	    return left && right;
+	    return preorderInvCheck(t1.left, t2.right) &&
+                preorderInvCheck(t1.right, t2.left);
     }
 }
 
