413f5f66fb369918f876944e63723bf5c6b1ca8e
Stephan <stephan@branczyk.org>
2022-06-28 14:46:55 -0700

    Time: 266 ms (31.22%), Space: 159.1 MB (76.88%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,17 +1,17 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        HashMap<Integer, HashSet<Integer>> adj = new HashMap();
-        for (int i = 0; i < n; i++) adj.put(i, new HashSet());
+        HashMap<Integer, ArrayList<Integer>> adj = new HashMap();
+        for (int i = 0; i < n; i++) adj.put(i, new ArrayList<Integer>());
         for (int[] edge : edges) {
             adj.get(edge[0]).add(edge[1]);
             adj.get(edge[1]).add(edge[0]);
         }
         return dfs(adj, source, destination);
     }
-    private boolean dfs(HashMap<Integer, HashSet<Integer>> adj, int s, int t) {
+    private boolean dfs(HashMap<Integer, ArrayList<Integer>> adj, int s, int t) {
         if (s == t) return true;
         if (!adj.containsKey(s)) return false;
-        HashSet<Integer> currentList = adj.get(s);
+        ArrayList<Integer> currentList = adj.get(s);
         adj.remove(s);
         for(var node : currentList) {
             if (dfs(adj, (int)node, t)) return true;
