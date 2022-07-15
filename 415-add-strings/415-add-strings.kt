e5f78c0869aa2f5cf2a0957d960b953764700eb2
Stephan <stephan@branczyk.org>
2022-07-15 02:13:52 -0700

    Time: 263 ms (83.05%), Space: 37.5 MB (54.24%) - LeetHub

--- 415-add-strings/415-add-strings.kt
+++ 415-add-strings/415-add-strings.kt
@@ -1,28 +1,22 @@
 class Solution {
     fun addStrings(n1: String, n2: String): String {
         val sb = StringBuilder()
-        var i = 0
-        var remainder = 0
-        while (i <= n1.length || i <= n2.length) {
-            var res = remainder
-            if (i < n1.length) {
-                res += n1[n1.lastIndex-i]-'0'
-            }
-            if (i < n2.length) {
-                res += n2[n2.lastIndex-i]-'0'
-            }
-            if (res == 0) {
-                if (i > n1.lastIndex && i > n2.lastIndex) {
-                    break
-                }
-            }
-            remainder = 0
+        var i = n1.lastIndex
+        var j = n2.lastIndex
+        var carry = 0
+        while (i >= -1 || j >= -1) {
+            var res = carry
+            carry = 0
+            res += if (i < 0) 0 else n1[i]-'0'
+            res += if (j < 0) 0 else n2[j]-'0'
+            if (res == 0 && i < 0 && j < 0) break
             if (res > 9) {
-                remainder = res / 10
-                res = res % 10
+                carry = res / 10
+                res %= 10
             }
             sb.append(res)
-            i++
+            i--
+            j--
         }
         return sb.reverse().toString()
     }
