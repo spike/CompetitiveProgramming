387560792042836cee8fd8a8224749eda1723a70
Stephan <stephan@branczyk.org>
2022-07-16 00:29:53 -0700

    Time: 241 ms (72.34%), Space: 37.7 MB (21.28%) - LeetHub

---
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun isAlienSorted(words: Array<String>, order: String): Boolean {
+        val t = Array(26) {0}
+        for (i in order.indices) {
+            val c = order[i]
+            t[c-'a'] = i
+        }
+        for (i in 1..words.lastIndex) {
+            if (!words[i-1].isBefore(words[i], t)) return false
+        }
+        return true
+    }
+    fun String.isBefore(other: String, t: Array<Int>): Boolean {
+        val len = Math.min(this.length, other.length)
+        for (i in 0..len-1) {
+            if (t[this[i]-'a'] < t[other[i]-'a']) return true
+            if (t[this[i]-'a'] > t[other[i]-'a']) return false
+        }
+        return (this.length <= other.length)
+    }
+}
