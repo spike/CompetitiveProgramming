913daec9058e646064c98d2fa30ce7e5b1901e7f
Stephan <stephan@branczyk.org>
2022-07-16 22:41:58 -0700

    Time: 200 ms (100.00%), Space: 35.7 MB (100.00%) - LeetHub

--- 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.kt
+++ 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.kt
@@ -1,5 +1,19 @@
 class Solution {
     fun numberOfPairs(nums: IntArray): IntArray {
+        val arr = Array(101) {0}
+        for (n in nums) {
+            arr[n]++
+        }
+        var leftOvers = 0
+        for (f in arr) {
+            if (f % 2 == 1) leftOvers++
+        }
+        return intArrayOf((nums.size-leftOvers)/2, leftOvers)
+    }
+}
+/*
+class Solution {
+    fun numberOfPairs(nums: IntArray): IntArray {
         val hm = HashMap<Int,Int>()
         for (n in nums) {
             hm.put(n, hm.getOrDefault(n, 0) + 1)
@@ -11,3 +25,4 @@ class Solution {
         return intArrayOf((nums.size-leftOvers)/2, leftOvers)
     }
 }
+*/
