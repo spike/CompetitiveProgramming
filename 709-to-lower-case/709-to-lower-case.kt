14d237a22120287bdc4f6a7200c35814393db885
Stephan <stephan@branczyk.org>
2022-07-17 04:06:00 -0700

    Time: 244 ms (30.16%), Space: 34.5 MB (38.10%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.kt
+++ 709-to-lower-case/709-to-lower-case.kt
@@ -1,3 +1,10 @@
 class Solution {
-    fun toLowerCase(s: String) = s.toLowerCase()
+    fun toLowerCase(s: String): String {
+        val sb = StringBuilder()
+        for (c in s) {
+            if (c in 'A'..'Z') sb.append(c+32)
+            else sb.append(c)
+        }
+        return sb.toString()
+    }
 }
