cbfe4e86cefa1accc6e40552a3de2a8b160444e7
Stephan <stephan@branczyk.org>
2022-07-17 04:29:19 -0700

    Time: 404 ms (50.00%), Space: 48.8 MB (33.96%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.kt
+++ 905-sort-array-by-parity/905-sort-array-by-parity.kt
@@ -2,9 +2,9 @@ class Solution {
     fun sortArrayByParity(nums: IntArray): IntArray {
         var l = 0; var r = nums.lastIndex
         while (l < r) {
-            if (nums[l]%2 == 0) l++
-            else if (nums[r]%2 == 1) r--
-            else if (nums[l]%2 != 0 && nums[r]%2 != 1) {
+            while (nums[l]%2 == 0 && l < r) l++
+            while (nums[r]%2 == 1 && l < r) r--
+            if (nums[l]%2 != 0 && nums[r]%2 != 1 && l < r) {
                 nums[l] = nums[r].also{ nums[r] = nums[l] }
                 l++; r--
             }
