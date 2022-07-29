e8f5f5ebfe283f71f9ec294dde3e094a81fef1df
Stephan <stephan@branczyk.org>
2022-07-28 21:39:43 -0700

    Time: 241 ms (44.52%), Space: 33.7 MB (83.21%) - LeetHub

--- 202-happy-number/202-happy-number.kt
+++ 202-happy-number/202-happy-number.kt
@@ -1,15 +1,14 @@
 class Solution {
     fun isHappy(n: Int): Boolean {
-        var attempts = 1000
+        val hs = HashSet<Int>()
         var num = n
-        while (attempts > 0 && num != 1) {
+        while (hs.add(num) && num != 1) {
             var total = 0
             while (num > 0) {
                 total += (num % 10) * (num % 10)
                 num /= 10
             }
             num = total
-            attempts--
         }
         return (num == 1) 
     }
