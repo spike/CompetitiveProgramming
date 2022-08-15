cab9be6278a5013d0d5e22370c5ddde8822950c5
Stephan <stephan@branczyk.org>
2022-08-15 13:55:58 -0700

    Time: 1034 ms (14.74%), Space: 98.7 MB (19.84%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.kt
+++ 53-maximum-subarray/53-maximum-subarray.kt
@@ -1,25 +1,9 @@
-class Solution { /*
-- contiguous
-
-Brute force:
-- add up all the contiguous permutations together
-- exponential Big O
-- keep only the maximum sum
-
-Special case:
-- if all the numbers are negative, return the least negative one
-
-
-- start by adding up all the contiguous positive numbers together
-- if a negative number is found after positive numbers, see if the substraction
-doesn't cancel out the negative number
-- keep only the maximum sum
-
-*/  fun maxSubArray(nums: IntArray): Int {  // [-9, -2]
-        var max = Integer.MIN_VALUE
+class Solution {  
+    fun maxSubArray(nums: IntArray): Int { 
+        var max = nums[0]!!
         var sum = 0
         var positive = false
-        for (n in nums) {  // 
+        for (n in nums) {  
             if (n >= 0) {
                 sum += n
                 max = Math.max(sum, max)
