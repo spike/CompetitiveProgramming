dbfa6c03dd6cbbc45a679612d81b6b1393d9baa5
Stephan <stephan@branczyk.org>
2022-09-01 18:54:29 -0700

    Time: 1156 ms (55.56%), Space: 76.4 MB (55.56%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.kt
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.kt
@@ -1,4 +1,13 @@
 class Solution {
+    /*
+    [5,4,9], k = 2    total = 18
+    9- (9/2)= 5
+    [5,4,5]
+    5 - (5/2)=3
+    [3,4,5] = 12
+
+    */
+    
     fun minStoneSum(piles: IntArray, k: Int): Int {
         var countDown = k
         val pq = PriorityQueue<Int>(Collections.reverseOrder())
@@ -7,6 +16,8 @@ class Solution {
             pq.offer(p)
             total += p
         }
+        // [5,4,9]
+        // [9,5,4]
         while (pq.isNotEmpty() && countDown > 0) {
             val biggest = pq.remove()
             val newPile: Int = biggest - (biggest/2)
