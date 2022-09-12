c1f21aa32eeb415a5f20e13366fdeaeb8d873878
Stephan <stephan@branczyk.org>
2022-09-12 16:21:03 -0700

    Time: 312 ms (5.17%), Space: 35.4 MB (5.03%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.kt
+++ 70-climbing-stairs/70-climbing-stairs.kt
@@ -1,11 +1,28 @@
 class Solution {
+    lateinit var dp: Array<Int>
+    
     fun climbStairs(n: Int): Int {
-        val dp = Array(n+1) {0}
+        dp = Array(n+1) {0}
+      //  return topDown(n)
+        return bottomUp(n)
+      // return iterativeClimbStairs(n)
+    }
+    
+    fun bottomUp(n: Int): Int {
         dp[0] = 1
         dp[1] = 1
         for (i in 2..n) {
             dp[i] = dp[i-2] + dp[i-1]
         }
         return dp[dp.lastIndex]
+    }
+    
+    fun topDown(n: Int): Int {
+        if (n <= 1) return 1
+        if (dp[n] > 0) return dp[n]
+        var sum = topDown(n-1)
+        sum += topDown(n-2)
+        dp[n] = sum
+        return sum
     }
 }
