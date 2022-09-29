4b13564e4af013584f773794077a807481075791
Stephan Branczyk <stephan@branczyk.org>
2022-09-29 13:16:46 -0700

    Time: 166 ms (88.89%), Space: 35.1 MB (33.33%) - LeetHub

--- 925-long-pressed-name/925-long-pressed-name.kt
+++ 925-long-pressed-name/925-long-pressed-name.kt
@@ -1,31 +1,41 @@
 class Solution {
     fun isLongPressedName(name: String, typed: String): Boolean {
-        val (n1, n2) = chop(name)
-        val (t1, t2) = chop(typed)
+        val (n1, n2) = huffmanEncode(name) 
+        // this returns two stacks (2 deques) n1 and n2
+        val (t1, t2) = huffmanEncode(typed)
+        // this returns two stacks (2 deques) t1 and t2
+        // t1 is the stack of characters and t2 is the stack of counts
+        
         while (!n1.isEmpty() && !t1.isEmpty()) {
-            if (n1.pollFirst() != t1.pollFirst()) {
-                return false
-            }
-            if (n2.pollFirst() > t2.pollFirst()) {
-                return false
-            }
+            // if letter is different return false
+            if (n1.pollLast() != t1.pollLast()) return false
+            // if count of letter in name > count of letter in typed
+            // return false
+            if (n2.pollLast() > t2.pollLast()) return false
         }
         return (n1.isEmpty() && t1.isEmpty())
     }
-    fun chop(str: String): Pair<ArrayDeque<Char>,ArrayDeque<Int>> {
-        val q1 = ArrayDeque<Char>()
-        val q2 = ArrayDeque<Int>()
-        
-        // if letter is not in q1, add in q1 and add in q2
-        // if letter is in q1, removeLast and increment, and addlast
+    
+    // Huffman encoding means "sssaeed" => 3s,1a,2e,1d
+    // but in my case, I'm using two stacks
+    // str="sssaeed"   => stackChar = { 's', 'a', 'e', 'd'}
+    //                    stackCount= {  3 ,  1 ,  2 ,  1 }
+    
+    fun huffmanEncode(str: String): 
+                Pair<ArrayDeque<Char>,ArrayDeque<Int>> {
+        val stackChar = ArrayDeque<Char>()
+        val stackCount = ArrayDeque<Int>()
         for (i in str.indices) {
-            if (q1.isEmpty() || str[i] != q1.peekLast()) {
-                q1.addLast(str[i])
-                q2.addLast(1)
+        // if stack is empty or if current letter is not same as last letter
+        // add letter and add number to both stacks
+            if (stackChar.isEmpty() || str[i] != stackChar.peekLast()) {
+                stackChar.addLast(str[i])
+                stackCount.addLast(1)
             } else {
-                q2.addLast(q2.pollLast() + 1)
+                // else letter is same, replace count with count + 1
+                stackCount.addLast(stackCount.pollLast() + 1)
             }
         }
-        return Pair(q1, q2)
+        return Pair(stackChar, stackCount)
     }
 }
