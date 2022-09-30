d4964830177df6f4b9b006d9c9e0469419dad6fb
Stephan Branczyk <stephan@branczyk.org>
2022-09-30 15:58:05 -0700

    Time: 551 ms (12.45%), Space: 40.2 MB (71.66%) - LeetHub

--- 326-power-of-three/326-power-of-three.kt
+++ 326-power-of-three/326-power-of-three.kt
@@ -3,7 +3,6 @@ class Solution {
         if (k < 1) return false
         var n = k
         while (n > 1) {
-            println(n)
             if (n % 3 == 0) n /= 3
             else return false
         }
