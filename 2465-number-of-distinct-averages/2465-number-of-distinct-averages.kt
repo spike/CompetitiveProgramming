08a93688c8ea657540f71c44bc50e7160853fc75
Stephan Branczyk <stephan@branczyk.org>
2022-12-13 21:54:10 -0800

    Time: 179 ms (76.47%), Space: 35.4 MB (80.39%) - LeetHub

--- 2465-number-of-distinct-averages/2465-number-of-distinct-averages.kt
+++ 2465-number-of-distinct-averages/2465-number-of-distinct-averages.kt
@@ -3,7 +3,6 @@ class Solution {
         val hs = HashSet<Double>()
         nums.sort()
         var left = 0; var right = nums.lastIndex
-        var max = 0
         while(left < right) {
             hs.add((nums[left] + nums[right]) / 2.0)
             left++
