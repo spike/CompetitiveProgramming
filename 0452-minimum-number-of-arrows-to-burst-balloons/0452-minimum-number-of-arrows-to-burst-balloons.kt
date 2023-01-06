ee6cec73a36db4aa386f20ad337e26d934c8f881
Stephan Branczyk <stephan@branczyk.org>
2023-01-05 19:47:58 -0800

    Time: 2782 ms (6.45%), Space: 152 MB (6.45%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.kt
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.kt
@@ -3,27 +3,19 @@
 [1,6]
 ------
  -------
-     -----    -----
-     
-*/
-
-
-
+     -----    -----            */
 class Solution {
     fun findMinArrowShots(points: Array<IntArray>): Int {
-        points.sortWith(Comparator { a, b ->
-            a[1].compareTo(b[1])
-        })
+        points.sortBy { it[1] }
         var count = 1
         var prev = points[0][1]
         for (i in 1..points.lastIndex) {
             val p = points[i]
             if (prev >= p[0]) {
                 continue
-            } else {
-                count++
-                prev = p[1]
-            }
+            } 
+            count++
+            prev = p[1]
         }            
         return count
     }
