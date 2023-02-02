df7d404990e7648d5adbd366d1fdfa829e989a9e
Stephan Branczyk <stephan@branczyk.org>
2023-02-01 16:05:06 -0800

    Time: 177 ms (100.00%), Space: 35.5 MB (66.67%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.kt
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.kt
@@ -1,75 +1,14 @@
-//class Solution { /*
-/*
-gcd of lengths
-recursive
-"ABABAB", 
-str2 = "ABAB"
-ABAB
-largest
-frequency map
-*/
-//     fun gcdOfStrings(str1: String, str2: String): String {
-//         val l1 = str1.length; val l2 = str2.length
-//         val gcd = ArrayList<Int>()
-//         if (l1 > l2) {
-//             helper()
-//         } else  {
-            
-//         }
-//     }
-//     fun helper(p: String, end: Int, s: String) {
-        
-//     }
-//     fun gcd (n1: Int, n2: Int) {
-//         var i = 1
-//         var res = 0
-//         while (i <= n1 && i <= n2) {
-//           if (n1 % i == 0 && n2 % i == 0) {
-//             res = i
-//           }
-//           ++i
-//         }
-//         return res
-//     }
+class Solution { 
 
-// }
+    public fun gcdOfStrings(str1: String, str2: String): String {
+        if (!(str1+str2).equals(str2+str1))  return ""
 
-class Solution {
-    fun gcdOfStrings(str1: String, str2: String): String {
-        val minLength = Math.min(str1.length, str2.length)
-        
-        (minLength downTo 1).forEach { length ->
-            if (str1.length % length == 0 && str2.length % length == 0) {
-                val sub1 = str1.substring(0, length)
-                val sub2 = str2.substring(0, length)
-                
-                if (sub1 == sub2 && isValidDivisor(sub1, str1, str2))
-                    return sub1
-            }
-        }
-        
-        return ""
+        val gcdVal = gcd(str1.length , str2.length)
+        return str2.substring(0, gcdVal)
     }
-    
-    private fun isValidDivisor(divisor: String, str1: String, str2: String): Boolean {
-        val maxLength = Math.max(str1.length, str2.length)
-        var i = 0
 
-        while (i < maxLength - divisor.length) {
-            var j = i + divisor.length
-
-            while (j < maxLength) {
-                if ((i < str1.length && j < str1.length && str1[i] != str1[j])
-                    || (i < str2.length && j < str2.length && str2[i] != str2[j]))
-                    return false
-                
-                i++
-                j++
-            }
-
-            i++
-        }
-
-        return true
+    public fun gcd(p: Int, q: Int): Int {
+        if (q == 0) return p
+        else return gcd(q, p % q)
     }
 }
