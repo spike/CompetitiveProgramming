a72bdde05f3229b78009f04378d21ab74d826909
Stephan B <stephan@branczyk.org>
2023-05-29 00:04:12 -0700

    Time: 137 ms (62.17%), Space: 16.8 MB (38.14%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.py
+++ 1603-design-parking-system/1603-design-parking-system.py
@@ -1,15 +1,31 @@
-class ParkingSystem(var big: Int, var medium: Int, var small: Int) {
-    fun addCar(carType: Int): Boolean {
-        return when (carType) {
-                    1 -> if (big > 0) { big--; true } else false 
-                    2 -> if (medium > 0) { medium--; true } else false 
-                    else -> if (small > 0) { small--; true } else false 
-                }
-    }
-}
+class ParkingSystem:
+    def __init__(self, big: int, medium: int, small: int):
+        self.b = big
+        self.m = medium
+        self.s = small
 
-/**
- * Your ParkingSystem object will be instantiated and called as such:
- * var obj = ParkingSystem(big, medium, small)
- * var param_1 = obj.addCar(carType)
- */
+    def addCar(self, carType: int) -> bool:
+        if carType == 1: 
+            self.b -= 1
+            if self.b >= 0: 
+                return True 
+            else:
+                return False
+        if carType == 2:
+            self.m -= 1
+            if self.m >= 0:
+                return True
+            else: 
+                return False
+        if carType == 3:
+            self.s -= 1
+            if self.s >= 0:
+                return True
+            else: 
+                return False
+        
+            
+
+# Your ParkingSystem object will be instantiated and called as such:
+# obj = ParkingSystem(big, medium, small)
+# param_1 = obj.addCar(carType)
