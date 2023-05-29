bf965d3232c1ffebbe09ec70210a1ef155f64b24
Stephan B <stephan@branczyk.org>
2023-05-28 23:49:56 -0700

    Time: 444 ms (6.90%), Space: 83.2 MB (6.90%) - LeetHub

---
+++ 1603-design-parking-system/1603-design-parking-system.kt
@@ -0,0 +1,38 @@
+class ParkingSystem(var big: Int, var medium: Int, var small: Int) {
+    
+    fun addCar(carType: Int): Boolean {
+        return when (carType) {
+                    1 -> {
+                        if (big > 0) {
+                            big--
+                            true
+                        } else {
+                            false
+                        }
+                    }
+                    2 -> {
+                        if (medium > 0) {
+                            medium--
+                            true
+                        } else {
+                            false
+                        }
+                    }
+                    else -> {
+                        if (small > 0) {
+                            small--
+                            true
+                        } else {
+                            false
+                        }
+                    }
+                }
+    }
+
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * var obj = ParkingSystem(big, medium, small)
+ * var param_1 = obj.addCar(carType)
+ */
