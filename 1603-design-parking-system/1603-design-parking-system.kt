192a1bdde1b2928e9c149c788e333ab9e381aa99
Stephan B <stephan@branczyk.org>
2023-05-28 23:54:56 -0700

    Time: 354 ms (34.48%), Space: 74.4 MB (48.28%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.kt
+++ 1603-design-parking-system/1603-design-parking-system.kt
@@ -1,9 +1,9 @@
 class ParkingSystem(var big: Int, var medium: Int, var small: Int) {
     fun addCar(carType: Int): Boolean {
         return when (carType) {
-                    1 -> { if (big > 0) { big--; true } else false }
-                    2 -> { if (medium > 0) { medium--; true } else false }
-                    else -> { if (small > 0) { small--; true } else false }
+                    1 -> if (big > 0) { big--; true } else false 
+                    2 -> if (medium > 0) { medium--; true } else false 
+                    else -> if (small > 0) { small--; true } else false 
                 }
     }
 }
