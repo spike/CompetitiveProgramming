cbf146f69b1b9814c2119def20fbe43a0dbb5480
Stephan B <stephan@branczyk.org>
2023-06-04 22:01:48 -0700

    Time: 490 ms (40.00%), Space: 54.2 MB (80.00%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.kt
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.kt
@@ -3,14 +3,16 @@ class Solution {
         val usedLadders = PriorityQueue<Int>()
         var b = bricks
         var l = ladders
-        for (i in 1 until heights.size) {
+        for (i in 1..heights.lastIndex) {
             val diff = heights[i] - heights[i - 1]
             if (diff > 0) {
                 if (l > 0) {
                     usedLadders.offer(diff)
                     l--
                 } else {
-                    if (usedLadders.isNotEmpty() && usedLadders.peek() < diff && usedLadders.peek() <= b) {
+                    if (usedLadders.isNotEmpty() && 
+                            usedLadders.peek() < diff &&
+                            usedLadders.peek() <= b) {
                         b -= usedLadders.remove()
                         usedLadders.offer(diff)
                     } else if (b >= diff) {
