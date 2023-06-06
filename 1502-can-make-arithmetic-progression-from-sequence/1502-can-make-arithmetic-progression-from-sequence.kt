99a4fc5f997b942e0545dbb12dd1bee3b62f9653
Stephan B <stephan@branczyk.org>
2023-06-05 23:02:21 -0700

    Time: 187 ms (33.33%), Space: 36.7 MB (66.67%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.kt
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.kt
@@ -1,9 +1,9 @@
 class Solution {
     fun canMakeArithmeticProgression(arr: IntArray): Boolean {
         arr.sort()
-        var diff = Math.abs(arr[1] - arr[0])
+        var diff = arr[1] - arr[0]
         for (i in 1..arr.lastIndex) {
-            if (diff != Math.abs(arr[i] - arr[i-1])) {
+            if (diff != arr[i] - arr[i-1]) {
                 return false
             }
         }
