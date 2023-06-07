5d4bc1ba187bba679cbe999ca6857b64573ba807
Stephan B <stephan@branczyk.org>
2023-06-06 18:50:46 -0700

    Time: 223 ms (39.28%), Space: 38.4 MB (46.43%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.kt
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.kt
@@ -9,32 +9,20 @@
 
 class Solution {
     fun connect(root: Node?): Node? {
-        val q1: Queue<Node> = LinkedList<Node>()
-        val q2: Queue<Node> = LinkedList<Node>()
+        var q1: Queue<Node> = LinkedList<Node>()
+        var q2: Queue<Node> = LinkedList<Node>()
         if (root == null) return root
         q1.add(root) 
-        var oddLevel = true
         while (q1.isNotEmpty() || q2.isNotEmpty()) {
-            if (oddLevel) {
-                var prev: Node? = null
-                while (q1.isNotEmpty()) {
-                    val node = q1.remove()
-                    node?.next = prev
-                    prev = node
-                    if (node!!.right != null) q2.add(node!!.right)
-                    if (node!!.left != null) q2.add(node!!.left)
-                }
-            } else {
-                var prev: Node? = null
-                while (q2.isNotEmpty()) {
-                    val node = q2.remove()
-                    node?.next = prev
-                    prev = node
-                    if (node!!.right != null) q1.add(node!!.right)
-                    if (node!!.left != null) q1.add(node!!.left)
-                }   
+            var prev: Node? = null
+            while (q1.isNotEmpty()) {
+                val node = q1.remove()!!
+                node.next = prev
+                prev = node
+                if (node.right != null) q2.add(node.right)
+                if (node.left != null) q2.add(node.left)
             }
-            oddLevel = !oddLevel
+            q1 = q2.also{ q2 = q1 }
         }
         return root
     }
