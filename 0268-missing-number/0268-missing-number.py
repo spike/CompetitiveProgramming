d7a974b6d69e94641c75eec38efc92d335f7b6e1
Stephan B <stephan@branczyk.org>
2023-06-23 15:53:54 -0700

    Time: 159 ms (25.47%), Space: 17.9 MB (19.18%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,9 +1,26 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        return self.missingNumberHole1(nums)
+        # return self.missingNumberSet(nums)
+        
+    def missingNumberHole1(self, nums: List[int]) -> int:
+        nums.sort()
+        N = len(nums)
+        for i in range(N):
+            if i != nums[i]:
+                return i
+        return N
+    # T: O(n log(n)) S: O(1)
+    # Using pigeonhole principle
+    
+    def missingNumberSet(self, nums: List[int]) -> int:
         num_set = {n for n in nums}
         for i in range(len(num_set)):
             if i not in num_set:
                 return i
         return len(nums)
+    
+    # T: O(n) S: O(n) for using set for storage
+    # O(1) space is also possible using pigeon hole principle
             
         
