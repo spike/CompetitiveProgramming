e4194dedb4d0b5f1acde31bb4ca453ebfffa299d
Stephan B <stephan@branczyk.org>
2023-07-02 19:43:56 -0700

    Time: 34 ms (99.73%), Space: 16.3 MB (25.93%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,9 +1,12 @@
 class Solution:
+    # 
+    
     def arrangeCoins(self, n: int) -> int:
-        inc = 1
-        while n > 0:
-            inc += 1
-            n -= inc
-        return inc-1
-
-        
+        lo, hi = -1, n+1
+        while hi - lo > 1:
+            m = (lo+hi)//2
+            if n >= (m * (m+1)// 2):
+                lo = m
+            else:
+                hi = m
+        return lo
