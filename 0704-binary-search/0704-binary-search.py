d325b6155470fac4f9142d44008395a00e9d8190
Stephan B <stephan@branczyk.org>
2023-07-17 19:49:04 -0700

    Time: 249 ms (47.67%), Space: 17.9 MB (34.74%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,11 +1,19 @@
 class Solution:
+    #  [-1,0,3,5,9,12]        lo = -1 
+    #.   0,1,2,3,4,5          hi = 6,
+    # [_,T,T,T,T,T,F,_]     
+    #            l
+    #              h
     def search(self, nums: List[int], target: int) -> int:
-        lo, hi = -1, len(nums)
-        while hi-lo > 1:
-            m = (lo + hi)//2
-            if nums[m] <= target:
-                lo = m
+        lo = -1
+        hi =  len(nums)  
+        while hi-lo > 1:   # 6+1 = 7 > 1 True
+            mid = (hi+lo)//2 
+            if nums[mid] <= target:
+                lo = mid
             else:
-                hi = m
-        return lo if nums[lo] == target else -1
+                hi = mid
+        return lo if (nums[lo] == target) else -1
+        
+        
         
