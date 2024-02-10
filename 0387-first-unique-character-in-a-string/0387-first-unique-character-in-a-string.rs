bde5918f85d6cc4b9233f92edb1f589b8799b790
Stephan B <stephan@branczyk.org>
2024-02-09 16:28:59 -0800

    Time: 2 ms (85.63%), Space: 2.2 MB (21.95%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.rs
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.rs
@@ -1,12 +1,12 @@
 impl Solution {
     pub fn first_uniq_char(s: String) -> i32 {
-        let mut arr = [0;26];
+        let mut arr = [0; 26];
         for c in s.bytes() {
-            arr[(c - b'a') as usize] += 1;
+            arr[(c-b'a') as usize] += 1;    
         }
-        for (i,c) in s.bytes().enumerate() {
-            if arr[(c-b'a') as usize] == 1 {
-                return i as i32;
+        for (i, c) in s.bytes().enumerate() {
+            if (arr[(c-b'a') as usize] == 1) {
+                return i as i32
             }
         }
         -1
