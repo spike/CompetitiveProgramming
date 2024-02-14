7d757007bd066301de5d574e95bd7affac52bafd
Stephan B <stephan@branczyk.org>
2024-02-13 23:27:12 -0800

    Time: 213 ms (72.77%), Space: 36.8 MB (64.80%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.kt
+++ 0009-palindrome-number/0009-palindrome-number.kt
@@ -1,6 +1,16 @@
 class Solution {
-    fun isPalindrome(n: Int): Boolean {
-        val sb = StringBuilder(n.toString())
-        return sb.toString().equals(sb.reverse().toString())
+    fun isPalindrome(x: Int): Boolean {
+        if (x < 0) return false
+        if (x == 0) return true
+        val lst = ArrayList<Int>()
+        var n = x
+        while (n > 0) {
+            lst.add(n % 10)
+            n /= 10
+        }
+        for (i in 0..lst.lastIndex/2) {
+            if (lst[i] != lst[lst.lastIndex-i]) return false
+        }
+        return true
     }
 }
