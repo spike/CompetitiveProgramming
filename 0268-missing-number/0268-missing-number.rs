0b39d8737db94782dceb00c74bd89efe2e39a72e
Stephan B <stephan@branczyk.org>
2024-02-20 03:10:03 -0800

    Time: 5 ms (15.25%), Space: 2.3 MB (60.54%) - LeetHub

--- 0268-missing-number/0268-missing-number.rs
+++ 0268-missing-number/0268-missing-number.rs
@@ -4,7 +4,7 @@ impl Solution {
     pub fn missing_number(nums: Vec<i32>) -> i32 {
         let mut hs: HashSet<i32> = HashSet::new();
         let len = nums.len();
-        for (i, n) in nums.iter().enumerate() {
+        for n in nums.iter() {
             hs.insert(*n);
         }
         for i in 0..len+1 {
