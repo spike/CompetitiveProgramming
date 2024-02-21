adae5c8e0609c6612ef54cf2f24cc207d2daf892
Stephan B <stephan@branczyk.org>
2024-02-20 20:54:31 -0800

    Time: 0 ms (100.00%), Space: 2.1 MB (38.38%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.rs
+++ 0058-length-of-last-word/0058-length-of-last-word.rs
@@ -1,15 +1,10 @@
 impl Solution {
     pub fn length_of_last_word(s: String) -> i32 {
-        let mut res = 0;
-        let mut count = 0;
-        for c in s.bytes() {
-            if c != b' ' {
-                count += 1;
-                res = count;
-            } else {
-                count = 0;
-            }
-        }
-        res
+        s.trim()
+            .split_whitespace()
+            .collect::<Vec<&str>>()
+            .last()
+            .unwrap()
+            .len() as i32
     }
 }
