c60b068e8660910f230728cdf1c6f4d71a1a4167
Stephan B <stephan@branczyk.org>
2024-02-24 12:25:08 -0800

    Time: 1 ms (78.61%), Space: 2.3 MB (20.40%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn maximum_wealth(accounts: Vec<Vec<i32>>) -> i32 {
+        let mut richest: i32 = 0;
+        
+        for customer in accounts {
+            let mut total = 0;
+            for money in customer {
+                total += money;
+            }
+            if total > richest {
+                richest = total;
+            }
+        }
+        richest
+    }
+}
