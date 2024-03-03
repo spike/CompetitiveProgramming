1a3e3f1c35802ab424b811a733979212327d2a3c
Stephan B <stephan@branczyk.org>
2024-03-03 14:34:15 -0800

    Time: 1 ms (80.75%), Space: 2.2 MB (76.53%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.rs
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.rs
@@ -1,16 +1,15 @@
-use std::cmp::max;
+use std::cmp;
 
 impl Solution {
     pub fn maximum_wealth(accounts: Vec<Vec<i32>>) -> i32 {
-        let mut richest: i32 = 0;
-        
-        for customer in accounts {
+        let mut max = 0;
+        for person in accounts {
             let mut total = 0;
-            for money in customer {
-                total += money;
+            for n in person {
+                total += n
             }
-            richest = max(total, richest);
+            max = cmp::max(total, max);
         }
-        richest
+        max
     }
 }
