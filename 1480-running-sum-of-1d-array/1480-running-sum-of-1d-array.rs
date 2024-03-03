6173ff1079780872e9b1286993cb9ce2ae7a9772
Stephan B <stephan@branczyk.org>
2024-03-03 14:14:00 -0800

    Time: 1 ms (83.10%), Space: 2.1 MB (95.42%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.rs
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.rs
@@ -1,11 +1,11 @@
 impl Solution {
     pub fn running_sum(nums: Vec<i32>) -> Vec<i32> {
-        let mut res: Vec<i32> = Vec::new();
-        let mut tot = 0;
+        let mut v: Vec<i32> = Vec::new();
+        let mut total = 0i32;
         for n in nums {
-            tot += n;
-            res.push(tot);
+            total += n;
+            v.push(total);
         }
-        res
+        v
     }
 }
